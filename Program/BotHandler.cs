3f565816cb5390229a793fc2cffd25c2fc5e2697
Maxim Osin <maxim.neitralov@gmail.com>
2022-08-26 19:04:49 +0700

    Блок кода выполняющий команды боту вынесен в метод ProcessComandAsync

--- Program/BotHandler.cs
+++ Program/BotHandler.cs
@@ -30,37 +30,8 @@ public static class BotHandler
 
         Log.Info($"Получено сообщение '{messageText}' из чата {chatId}.");
 
-        messageText = messageText.Split('@')[0];
-
-        const string startMessage = "Это бот, который отправляет расписание БГК.\n\n" +
-                                    "Получите изображение с актуальным расписанием!\n" +
-                                    "/get1 | Первый корпус.\n" +
-                                    "/get2 | Второй корпус.\n" +
-                                    "/get3 | Третий корпус.\n" +
-                                    "/get4 | Четвертый корпус.\n\n" +
-                                    "Подпишитесь на рассылку новых расписаний!\n" +
-                                    "/subscribe1 | Первый корпус.\n" +
-                                    "/subscribe2 | Второй корпус.\n" +
-                                    "/subscribe3 | Третий корпус.\n" +
-                                    "/subscribe4 | Четвертый корпус.\n\n" +
-                                    "/unsubscribe | Отписаться от всех подписок.";
-
-        var task = messageText switch
-        {
-            "/start" => botClient.SendTextMessageAsync(chatId, startMessage, cancellationToken: cancellationToken),
-            "/get1" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.First),
-            "/get2" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.Second),
-            "/get3" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.Third),
-            "/get4" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.Fourth),
-            "/subscribe1" => Notifier.AddSubscriberAsync(chatId, Corps.First),
-            "/subscribe2" => Notifier.AddSubscriberAsync(chatId, Corps.Second),
-            "/subscribe3" => Notifier.AddSubscriberAsync(chatId, Corps.Third),
-            "/subscribe4" => Notifier.AddSubscriberAsync(chatId, Corps.Fourth),
-            "/unsubscribe" => Notifier.RemoveSubscriberAsync(chatId),
-            _ => Task.CompletedTask
-        };
-
-        await task;
+        var command = messageText.Split('@')[0];
+        await ProcessCommandAsync(command, chatId);
     }
 
     private static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception,
@@ -75,5 +46,42 @@ public static class BotHandler
 
         Console.WriteLine(errorMessage);
         return Task.CompletedTask;
+    }
+
+    /// <summary>Выполняет команду, которую отправили боту.</summary>
+    /// <param name="command">Текст команды.</param>
+    /// <param name="chatId">TelegramID с которого отправили сообщение.</param>
+    /// <remarks>При отправке несуществующей команды игнорирует её.</remarks>
+    private static async Task ProcessCommandAsync(string command, long chatId)
+    {
+        const string startMessage = "Это бот, который отправляет расписание БГК.\n\n" +
+                                    "Получите изображение с актуальным расписанием!\n" +
+                                    "/get1 | Первый корпус.\n" +
+                                    "/get2 | Второй корпус.\n" +
+                                    "/get3 | Третий корпус.\n" +
+                                    "/get4 | Четвертый корпус.\n\n" +
+                                    "Подпишитесь на рассылку новых расписаний!\n" +
+                                    "/subscribe1 | Первый корпус.\n" +
+                                    "/subscribe2 | Второй корпус.\n" +
+                                    "/subscribe3 | Третий корпус.\n" +
+                                    "/subscribe4 | Четвертый корпус.\n\n" +
+                                    "/unsubscribe | Отписаться от всех подписок.";
+
+        var task = command switch
+        {
+            "/start" => BotClient.SendTextMessageAsync(chatId, startMessage),
+            "/get1" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.First),
+            "/get2" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.Second),
+            "/get3" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.Third),
+            "/get4" => ScheduleFinder.SendSchedulePictureAsync(chatId, Corps.Fourth),
+            "/subscribe1" => Notifier.AddSubscriberAsync(chatId, Corps.First),
+            "/subscribe2" => Notifier.AddSubscriberAsync(chatId, Corps.Second),
+            "/subscribe3" => Notifier.AddSubscriberAsync(chatId, Corps.Third),
+            "/subscribe4" => Notifier.AddSubscriberAsync(chatId, Corps.Fourth),
+            "/unsubscribe" => Notifier.RemoveSubscriberAsync(chatId),
+            _ => Task.CompletedTask
+        };
+
+        await task;    
     }
 }
