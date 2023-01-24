027f17b77c87509f1990f1d7f06bb0a8c8254bd3
Neitralov <maxim.neitralov@gmail.com>
2023-01-24 13:12:32 +0700

    Переход с PostgreSQL к SQLite. Обновлены зависимости.

--- DataBase/DataBaseProvider.cs
+++ DataBase/DataBaseProvider.cs
@@ -11,11 +11,6 @@ public sealed class DataBaseProvider : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var dataBaseName = Environment.GetEnvironmentVariable("POSTGRES_DB");
-        var userName = Environment.GetEnvironmentVariable("POSTGRES_USER");
-        var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-
-        var connectionString = $"Host=db;Port=5432;Database={dataBaseName};Username={userName};Password={password}";
-        optionsBuilder.UseNpgsql(connectionString);
+        optionsBuilder.UseSqlite("Data Source=DataBase.db");
     }
 }
